ea5b77e0b63fc0a098399cec55fad0be9ccda6b4
湯遒之 <nauxscript@gmail.com>
2023-11-09 18:34:00 +0800

    chore: add chakra-ui integration (#5)
    * chore: add chakra-ui integration
    
    closed #3
    
    * feat: adjust styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 
+import { Box, ListItem, UnorderedList } from "@chakra-ui/react";
 import { CreateTodo } from "~/app/_components/create-todo";
 import { api } from "~/trpc/server";
 
@@ -18,11 +19,13 @@ async function CrudShowcase() {
   const todos = await api.todo.list.query();
 
   return (
-    <div>
-      {
-        todos.map(todo => <div key={todo.id}>{todo.name}</div>)
-      }
-      <CreateTodo />
-    </div>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <UnorderedList marginX="10" marginY="5" width={300}>
+          {
+            todos.map(todo => <ListItem key={todo.id}>{todo.name}</ListItem>)
+          }
+        </UnorderedList> 
+        <CreateTodo />
+      </Box>
   );
 }
