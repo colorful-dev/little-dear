74720a5ab29160990a06d34d6a4a925ec4f32cb1
nauxscript <nauxscript@gmail.com>
2023-11-16 09:58:50 +0800

    fix: validate the values of signup form

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -14,19 +14,26 @@ import { useForm } from "react-hook-form";
 import React from "react";
 import { api } from "~/trpc/react";
 import { useRouter } from "next/navigation";
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
 
-type FormValues = {
-  phone: string;
-  password: string;
-  confirmPassword: string;
-};
+const schema = z.object({
+  phone: z.string().length(11, { message: 'invalid phone number'}),
+  password: z.string().min(6, 'Minimum 6 digits').max(16, 'Maximum 16 digits'),
+  confirmPassword: z.string()
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "Comfirm passwords don't match",
+  path: ['confirmPassword']
+})
 
 export default function SignUpPage() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormValues>();
+  } = useForm<typeof schema._type>({
+    resolver: zodResolver(schema)
+  });
 
   const router = useRouter();
   const toast = useToast();
