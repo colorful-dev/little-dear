04db569417c360246f525476cc74990c488802aa
Wecle <837968732@qq.com>
2023-11-22 00:01:34 +0800

    feat: add subcategory list drawer

--- src/app/add/_component/CategoryGrid.tsx
+++ src/app/add/_component/CategoryGrid.tsx
@@ -1,9 +1,10 @@
 'use client'
 
-import { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Grid, GridItem } from '@chakra-ui/react'
 import { Icon } from '@iconify/react'
 import IconItem from '../../_component/IconItem'
+import SubCategoryList, { type SubCategoryListRefProps } from './SubCategoryList'
 
 export interface IconInfo {
   categoryId: string | number
@@ -22,26 +23,31 @@ export interface CategoryGridProps {
 
 const CategoryGrid: React.FC<CategoryGridProps> = ({ icons, defaultValue = '', onChange, normalVariant = 'normal', activeVariant = 'danger' }) => {
   const [value, setValue] = useState(defaultValue)
+  const categoryRef = useRef<SubCategoryListRefProps>(null)
 
   const handleItemClick = (value: string | number, item: IconInfo) => {
     setValue(value)
     onChange && onChange(value, item)
+    item?.children?.length && categoryRef.current?.open()
   }
 
   return (
     <Grid templateColumns="repeat(5, 1fr)" rowGap={4}>
       {
-                icons.map((item, index) => {
-                  const isActive = value === item.categoryId
-                  const variant = isActive ? activeVariant : normalVariant
-                  const hasDetail = Array.isArray(item.children) && !!item.children.length
-                  return (
-                    <GridItem key={index} display="flex" flexDirection="column" onClick={() => handleItemClick(item.categoryId, item)}>
-                      <IconItem hasLabel={true} aria-label={item.label} variant={variant} icon={<Icon className="text-xl" icon={item.icon} />} hasDetail={hasDetail} />
-                    </GridItem>
-                  )
-                })
-            }
+        icons.map((item, index) => {
+          const isActive = value === item.categoryId
+          const variant = isActive ? activeVariant : normalVariant
+          const hasDetail = Array.isArray(item.children) && !!item.children.length
+          return (
+            <React.Fragment key={index}>
+              <GridItem display="flex" flexDirection="column" onClick={() => handleItemClick(item.categoryId, item)}>
+                <IconItem hasLabel={true} aria-label={item.label} variant={variant} icon={<Icon className="text-xl" icon={item.icon} />} hasDetail={hasDetail} />
+              </GridItem>
+              <SubCategoryList ref={categoryRef} list={item?.children || []} />
+            </React.Fragment>
+          )
+        })
+      }
     </Grid>
   )
 }
